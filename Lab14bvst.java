21712121e0b44c5b2dff068454be33b32dde12d3
rose-d <det9848@gmail.com>
2016-02-25 10:38:51 -0500

    Update Lab14bvst.java

--- Lab14bvst.java
+++ Lab14bvst.java
@@ -28,3 +28,20 @@ public class Lab14bvst extends Applet
 	}
 }
 	
+/*	public class Lab14bv100 extends Applet
+	{
+   	public void paint(Graphics g)
+   	{
+      	Train train = new Train(100,300);
+      	train.addCar("Locomotive",Color.blue);
+      	train.addCar("PassengerCar",Color.gray);
+      	train.addCar("PassengerCar",Color.gray);
+      	train.addCar("FreightCar",Color.green);
+      	train.addCar(3,"PassengerCar",Color.gray);
+      	train.addCar("FreightCar",Color.green);
+      	train.addCar("Caboose",Color.red);
+      	train.addCar(6,"FreightCar",Color.green);
+      	train.showCars(g);
+   	}
+	}
+*/	
