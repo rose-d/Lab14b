1ff085521c78ae546e4cc3e3d8a4ec56933d2865
rose-d <det9848@gmail.com>
2016-02-26 13:57:40 -0500

    Update Train.java

--- Train.java
+++ Train.java
@@ -1,22 +1,68 @@
+/*
+The Train class requires an addCar method that 
+
+adds a new RailCar object to a railCars dynamic array and places it at the end of the train. 
+
+The 100-Point version also has a second addCar method, which places a new RailCar object at a sequence number in the train. The first position, occupied by the locomotive is position 0.
+
+The Train class also needs a showCars method, 
+which displays the car in the sequence that the cars were added. 
+The very first car added to the Train object is displayed at the (x,y) coordinate that was provided with the instantiation of the new Train object.
+*/
+
+
+
+
+import java.util.*;
+import java.awt.*;
+import java.applet.*;
 public class Train
 {
    ArrayList <RailCar> railCars = new ArrayList<RailCar> ();
-
+   private int x;
+   private int y;
    
    public Train (int x, int y)      // int x = 100, int y = 300
    {
-      // railCar(Cc,100,300);
-      
+      this.x = x;
+      this.y = y;
    }
    
-   public void addCar(String type, Color color)  // 100 point version
-    {
+    public void addCar(String type, Color color)  
+    { 
       
+      if (type.indexOf("Locomotive")== 0)
+      {
+       RailCar locomotive = new Locomotive (color, x,y);   
+       x+=175;
+       railCars.add(locomotive);
+      }
+      
+       if (type.indexOf("PassengerCar")== 0)
+      {
+       RailCar passenger = new PassengerCar (color, x,y);   
+       x+=175;
+       railCars.add(passenger);
+      }
+      
+       if (type.indexOf("FreightCar")== 0)
+      {
+       RailCar freight = new FreightCar (color, x,y);   
+       x+=175;
+       railCars.add(freight);
+      }
+      
+       if (type.indexOf("Caboose")== 0)
+      {
+       RailCar caboose = new Caboose (color, x,y);   
+       x+=175;
+       railCars.add(caboose);
+      }
     }
  
     public void addCar(int location, String type, Color color)
     {
-   
+        
    
     }
 
@@ -25,29 +71,6 @@ public class Train
      for(RailCar car: railCars)
         {
            car.drawCar(g); 
-        }
-    }
-
-
-}
-
-   
-public void addCar(String type, Color color)  // 100 point version
-    {
-      
-    }
- 
-public void addCar(int location, String type, Color color)
-{
-   
-   
-}
-
-   public void showCars(Graphics g)
-    {
-     for(RailCar car: railCars)
-        {
-           drawCar(g); 
         }
     }
 
